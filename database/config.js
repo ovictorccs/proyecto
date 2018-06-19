6e09469f979fe797e55ca765cf780f055cf7d156
Ahiezer <ahiezerrivas@gmail.com>
2018-06-19 19:47:54 -0400

    Pruebas en base de datos de ahiezer

--- database/config.js
+++ database/config.js
@@ -2,7 +2,8 @@ var config = {
     host : 'localhost',
     user : 'root',
     // password : '',en blanco para las otras computadoras que no sean la de ahiezer
-    database : 'proyecto'
+    password:'root',
+    database : 'proyecto1'
 };
 
 module.exports = config;
